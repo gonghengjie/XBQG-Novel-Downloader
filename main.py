158e84d4bb3f86c162465b08f946a192c805ea4e
gonghengjie <46068573+gonghengjie@users.noreply.github.com>
2022-04-03 14:51:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # -*- encoding: utf-8 -*-
 
-import search.contents,search.search
-import download.getChapter,download.whole
+import sys
+from search.contents import *
+from search.search import *
+from download.whole import *
+sys.path.append('..')
+from parse import *
 
-#DownloadWhole("https://www.xbiquge.la/79/79721")
+text=DownloadWhole(url).replace(u'\xa0',' ')
+print("正在写文件...")
+file=open(savepath,"w",encoding="utf-8")
+file.write(text)
+file.close()
